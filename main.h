2af697b90a79e7b7c93780ab072324b04cc2bae3
miguecoder <mmarqzm@gmail.com>
2022-03-21 23:14:36 -0700

    Changes in .h

--- main.h
+++ main.h
@@ -1,8 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
-#include <stdlib.h>
+#include <stdarg.h> /* va_list, va_start, va_end, va_arg, va_copy*/
+#include <stdlib.h> /* malloc, free*/
+#include <unistd.h> /* Write function*/
 
+/*Principal Prootype*/
 int _printf(const char *format, ...);
+
+/*Old functions that can help us*/
+int _strlen(char *s);
+
+/*Function to select a function acording a format*/
+choose_func
+
+
+/*New prototypes for printf acording a format*/
+char printf_char(char c);
 
 #endif 
